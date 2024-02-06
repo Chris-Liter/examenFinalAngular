812ddf3dd6517ed5fad577badb406d17293bd55a
Chris-Liter <136271203+Chris-Liter@users.noreply.github.com>
2024-02-06 10:02:24 -0500

    final

--- src/app/pages/listado/listado.component.ts
+++ src/app/pages/listado/listado.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from 'src/app/model/Cliente';
 import { Cuenta } from 'src/app/model/Cuenta';
+import { Jefe } from 'src/app/model/Jefe';
 import { SolicitudesService } from 'src/app/services/solicitudes.service';
 
 @Component({
@@ -12,6 +13,7 @@ export class ListadoComponent implements OnInit{
 
   clientes: Cliente [] = []
   cuenta: Cuenta [] = []
+  jefe: Jefe = new Jefe()
 
   constructor(private service: SolicitudesService){
     
@@ -20,6 +22,16 @@ export class ListadoComponent implements OnInit{
   ngOnInit(): void {
     this.service.getCliente().subscribe(data=>{
       this.clientes = data
+    })
+    this.service.getJefe().subscribe(data =>{
+      this.jefe.dni = data.dni
+      this.jefe.id = data.id
+      this.jefe.nombre = data.nombre
+      this.service.getCuenta(data.id).subscribe(dat =>{
+        this.jefe.monto = dat.monto
+        this.jefe.telefono = dat.telefono
+        localStorage.setItem("jefe", JSON.stringify(this.jefe))
+      })
     })
   }
 
